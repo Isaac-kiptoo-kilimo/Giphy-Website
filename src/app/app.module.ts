80fbdc7afd9a8bc265e0cdc3d75beecc3569f715
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-14 07:08:12 +0300

    feature:rendering my angular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { GiphyComponent } from './giphy/giphy.component';
+import { FormComponent } from './form/form.component';
+import { GiphyDetailComponent } from './giphy/giphy-detail/giphy-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GiphyComponent,
+    FormComponent,
+    GiphyDetailComponent
   ],
   imports: [
     BrowserModule,
